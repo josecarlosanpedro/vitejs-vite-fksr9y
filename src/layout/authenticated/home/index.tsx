bc0434e77ea81a678e80f82fe0c2ca7e6892a693
Jose Carlo San Pedro <josecarlosanpedro0302@gmail.com>
2024-07-22 23:02:11 +0800

    sample auth

--- src/layout/authenticated/home/index.tsx
+++ src/layout/authenticated/home/index.tsx
@@ -1,10 +1,12 @@
 import { clearToken } from "../../../utils/cookies";
 import { useNavigate } from 'react-router-dom';
+import { Notification } from "../../../utils/notifications";
 
 function Home() {
     const navigate = useNavigate();
     const handleLogout =() =>  {
         clearToken() 
+        Notification('Successfully Logout')
         navigate('/')
     }
     return (
